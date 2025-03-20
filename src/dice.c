453b6e7ff653d2c60b01f46eb35be82b978e8a43
Thiago-A-Esteves <121464376+Thiago-A-Esteves@users.noreply.github.com>
2025-03-20 10:22:32 -0300

    Update dice.c

--- src/dice.c
+++ src/dice.c
@@ -6,12 +6,10 @@
 int roll_dice() {
     int dado1, dado2, dado3, soma;
 
-    // Gera valores aleatórios entre 1 e 6 para cada dado
     dado1 = (rand() % 6) + 1;
     dado2 = (rand() % 6) + 1;
     dado3 = (rand() % 6) + 1;
 
-    // Calcula a soma dos três dados
     soma = dado1 + dado2 + dado3;
 
     return soma;
